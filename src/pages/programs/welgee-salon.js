25627d4c68fda63e194c58f9e0e6fdab8ba34d7c
rhmtrz <rezaeihazara@gmail.com>
2019-12-12 17:22:04 +0900

    feat: welgee salon page

--- src/pages/programs/welgee-salon.js
+++ src/pages/programs/welgee-salon.js
@@ -3,15 +3,22 @@ import {Link} from "gatsby"
 import SectionDescription from "../../components/common/section-description"
 import AboutSectionTitle from "../../components/common/about-section-title"
 import Layout from "../../components/layout"
+import TextImageBackground from "../../components/common/text-img-background"
+import SupporterMember from "../../components/about/supporters/supporter"
+import SectionTitle from "../../components/common/section-title"
+
+import nextSalonImage from "../../images/about/director-message/join-donation.jpg"
+import YellowButton from "../../components/common/yellow.button"
+
 
 const salonData = [
   {
-    title: '難民と「ともに」<br/>つくる場',
+    title: '難民と「ともに」\n つくる場',
     image: 'http://welgee.sakura.ne.jp/image/07252019%20WELgee%20Salon-8351.JPG',
     description: '私たちは、毎回のイベントをinternationalsと共に設計しています。彼らは参加者でもあり、日本人の参加者を迎えるホストでもあります。'
   },
   {
-    title: '難民と「ともに」<br/>かたる場<',
+    title: '難民と「ともに」\n かたる場<',
     image: 'http://welgee.sakura.ne.jp/image/06002019-6482.JPG',
     description: 'WELgeeサロンは「難民について勉強する」一方的な場ではありません。参加者の皆さんとインターナショナルズとの双方向的なコミュニケーションを大切にしています。'
   },
@@ -25,32 +32,58 @@ const salonData = [
 const salonVol = [
   "vol.1 ドイツ視察報告会",
   "vol.15難民×家",
-"vol.2 世界にいる難民の人びと",
-"vol.16 「難民コミュニティ」との多文化共生",
-"vol.3 日本の難民",
-"vol.17 :伝える",
-"vol.4 日本にいるクルドの人びと",
-"vol.18 新生活",
-"vol.5 世界の難民について",
-"vol.19 日本とドイツの難民支援の違い",
-"vol.6 ホームステイを実際に経験した人の話を聞こう",
-"vol.20 ベルリン・パリ視察報告会",
-"vol.7 難民の日本での就労について考える",
-"vol.21 H.I.S.ドイツツアー事前研修",
-"vol.8 一人の難民がぶつかった課題についてみんなで考える",
-"vol.22 H.I.S. 旅あと研修",
-"vol.9 日本の難民問題・社会課題のアイデアソン",
-"vol.23 異文化間友情",
-"vol.10 H.I.S.共催 チュニジアについて",
-"vol.24 クリスマス交流会",
-"vol.11 難民ホームステイ報告会2弾",
-"vol.25 交流会",
-"vol.12 じっくり難民の方の話を聞くセッション",
-"vol.26 描くことで「自分のなりたい自分」を表現しよう",
-"vol.13 NPO法人コンフロントワールド共催",
-"vol.28 教育（日本の教育について）",
-"vol.14 農業ワークキャンプの報告",
-"vol.29 一歩（世界難民の日特別版）",
+  "vol.2 世界にいる難民の人びと",
+  "vol.16 「難民コミュニティ」との多文化共生",
+  "vol.3 日本の難民",
+  "vol.17 :伝える",
+  "vol.4 日本にいるクルドの人びと",
+  "vol.18 新生活",
+  "vol.5 世界の難民について",
+  "vol.19 日本とドイツの難民支援の違い",
+  "vol.6 ホームステイを実際に経験した人の話を聞こう",
+  "vol.20 ベルリン・パリ視察報告会",
+  "vol.7 難民の日本での就労について考える",
+  "vol.21 H.I.S.ドイツツアー事前研修",
+  "vol.8 一人の難民がぶつかった課題についてみんなで考える",
+  "vol.22 H.I.S. 旅あと研修",
+  "vol.9 日本の難民問題・社会課題のアイデアソン",
+  "vol.23 異文化間友情",
+  "vol.10 H.I.S.共催 チュニジアについて",
+  "vol.24 クリスマス交流会",
+  "vol.11 難民ホームステイ報告会2弾",
+  "vol.25 交流会",
+  "vol.12 じっくり難民の方の話を聞くセッション",
+  "vol.26 描くことで「自分のなりたい自分」を表現しよう",
+  "vol.13 NPO法人コンフロントワールド共催",
+  "vol.28 教育（日本の教育について）",
+  "vol.14 農業ワークキャンプの報告",
+  "vol.29 一歩（世界難民の日特別版）",
+]
+
+const voiceOfJapaneseDescription = "「アフリカでは困っている他人を躊躇なく救うという文化があると聞き、そんな彼らを日本社会としても、私個人としても、暖かく迎えたいと思いました。ー大学生 女性 20代」\n \n「僕はWELgeeにとても感謝しているよ。\nだって日本に来て初めてホームにいると感じた場所がサロンだ。—カメルーン人 男性 30代」"
+
+
+
+
+const salonMemberMessages = [
+  {
+    name: '市來 海',
+    image: 'http://welgee.sakura.ne.jp/image/kai-ichiki.png',
+    position: 'WELgeeサロン 統括',
+    message: '2001年岐阜県生まれ。早稲田大学在学中。高2でニュージーランドに一年間の留学。さまざまなバックグラウンドを持つ人々と生活するなかで「対話」の大切さを知る。 ',
+  },
+  {
+    name: '渡辺早希',
+    image: 'http://welgee.sakura.ne.jp/image/saki-watanabe.png',
+    position: 'WELgeeサロン インターン',
+    message: '1997年生まれ。母語は津軽弁。宇都宮大学国際学部在学中。 大学1年時、初海外ヨルダンで難民と出会う。野球少女時代を思い出し、多様な人と言葉のキャッチボールを日々特訓中。',
+  },
+  {
+    name: '宇波 里緒',
+    image: 'http://welgee.sakura.ne.jp/image/rio-unami.png',
+    position: 'WELgeeサロン インターン',
+    message: '1997年富山生まれ。千葉大学国際教養学部在学中。研究テーマは「『難民』に対する支援と共走」。趣味は旅行、ダンス、スノーボード。好きなものは南国と雪山。',
+  },
 ]
 
 const WelgeeSalonPage = () => {
@@ -110,6 +143,50 @@ const WelgeeSalonPage = () => {
         <ul className="salon_programs">
           {salonVol.map((v, i) => <li key={i}>{v}</li>)}
         </ul>
+      </section>
+      <section className="contents">
+        <h2>WELgeeサロンの一例</h2>
+        <img className="scheme-image" src="http://welgee.sakura.ne.jp/image/saloon-vol28.png" />
+        <p>2019年6月に開催したWELgeeサロン vol.28、世界難民の日特別版では、「First Step」のコンセプトの元<br />
+          グループワークとして「劇」を作りました。国籍や年齢を超えた一つのチームが、共同作業を通じて、<br />
+          「難民」という言葉にくくられてしまっている人々の個人として顔を見てもらうことを目的としたワークショップを行いました。
+        </p>
+    
+        <img className="scheme-image" src="http://welgee.sakura.ne.jp/image/saloon-vol28-timeline.png" />
+        <p>WELgeeサロン vol.28のタイムライン</p>
+      </section>
+      <TextImageBackground
+        jpTitle="WELgeeサロン参加者の声"
+        enTitle="Voice of Japanese participants"
+        description={voiceOfJapaneseDescription}
+        backgroundImage="https://lightroom.adobe.com/v2c/spaces/43c595e478f64bad8d0055a7cd36868e/assets/eec9ed5874cf42ffa4a3133d60ab3ab0/revisions/ef175ba59d334825aab89b5885d9c539/renditions/8ffdb01082e23d8d3f6b27da64543aab"
+      />
+      <section className="contents">
+        <h2>WELgeeサロンチーム</h2>
+        {salonMemberMessages.map((member, i) => (
+          <SupporterMember memberImage={member.image} memberName={member.name} memberPosition={member.position} memberMessage={member.message}/>
+        ))}
+      </section>
+      <br />
+      <br />
+      <hr />
+      <br />
+      <br />
+      <section className="contents">
+        <div id="join">
+          <SectionTitle jpTitle="次回のWELgeeサロン" enTitle="Next WELgee Saloon!" />
+          <br />
+          <div className="item">
+            <img className="next-salon-image" src={nextSalonImage} />
+            <h3>イベント詳細・申し込み</h3>
+            <p>
+              こちらから、イベント詳細・申し込みをすることができます。
+            </p>
+            <br />
+            <br />
+            <YellowButton title="more" link="news/news12"/>
+          </div>
+        </div>
       </section>
       <style jsx>{`
         .contents {
@@ -185,6 +262,23 @@ const WelgeeSalonPage = () => {
           width: 48%;
           padding-right: 2rem;
           line-height: 1.8rem;
+        }
+        .scheme-image{
+          margin-top: 50px;
+          width: 100%;
+          text-align:center;
+        }
+        #partners > .questions {
+          display: table;
+          flex-wrap: wrap;
+          justify-content: space-between;
+          align-items: center;
+          width: 80%;
+          max-width: 1080px;
+          margin: 0 auto;
+        }
+        .next-salon-image {
+          width: 25rem;
         }
       `}</style>
     </Layout>
